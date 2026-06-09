2dfbb74142d38f04f39decf0958b8f6e0ba4a34e
anisaekaputrii00-star <anisaekaputrii00@gmail.com>
2026-06-09 11:16:50 +0700

    fungsi inorder

--- searchtree.cpp
+++ searchtree.cpp
@@ -4,55 +4,77 @@ using namespace std;
 
 class node
 {
-    public:
-        string info;
-        node *leftchild;
-        node *rightchild;
-        //construotor for node class
-        node(string i, node *l=NULL, node *r)
-        {
-            info=i;
-            leftchild=l;
-            rightchild=r;
-        }
+public:
+    string info;
+    node *leftchild;
+    node *rightchild;
+
+    //constructor for node class
+    node(string i, node *l, node *r)
+    {
+        info = i;
+        leftchild = l;
+        rightchild = r;
+    }
 };
 
-class binaryTree
+class binarytree
 {
-    public:
-    node *root; //Initially root is null
-    binaryTree()
+public:
+    node *root;
+
+    binarytree()
     {
-        root=NULL;
+        root = NULL;
     }
 
-    //insert a node in the binary search tree
     void insert(string element)
     {
-        node *newnode=new node(element, NULL, NULL);
+        node *newnode = new node(element, NULL, NULL);
         
         newnode->info = element;
         newnode->leftchild = NULL;
         newnode->rightchild = NULL;
-        
-        node *parent= NULL;
-        node *currentNode = NULL;
-       
-        
-        search(element, parent, currentNode);
-        
-        if(parent==NULL)
+
+        node *parent = NULL;
+        node *currentnode = NULL;
+
+        search(element, parent, currentnode);
+
+        if (parent == NULL)
         {
-            root=newnode;
+            root = newnode;
+            return;
         }
-        else if(element < parent->info)
+
+        if(element < parent->info)
         {
             parent->leftchild = newnode;
         }
-        else
+        else if(element > parent->info)
         {
             parent->rightchild = newnode;
         }
     }
-}   
 
+    // this function searches the current node of the specified node
+    // as well as the current node of its parent
+    void search(string element, node *&parent, node *&currentnode)
+    {
+        currentnode = root;
+        parent = NULL;
+
+        while (currentnode != NULL && currentnode->info != element)
+        {
+            parent = currentnode;
+
+            if (element < currentnode->info)
+                currentnode = currentnode->leftchild;
+            else
+                currentnode = currentnode->rightchild;
+        }
+    }
+
+    void inorder(node *ptr)
+    {
+    }
